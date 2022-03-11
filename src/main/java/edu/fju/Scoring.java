6fdae235643d9c4c39d5285a885174311048ad5d
ZhiYun <uouo911226@gmail.com.tw>
2022-03-11 15:27:03 +0800

    Scoring

--- src/main/java/edu/fju/Scoring.java
+++ src/main/java/edu/fju/Scoring.java
@@ -2,9 +2,14 @@ package edu.fju;
 
 public class Scoring {
     public static void main(String[] args) {
-        Students candy = new Students();
-        candy.names="Candy";
-        candy.math=60;
-        candy.english=88;
+        Students candy = new Students("Candy",80,50);
+        //candy.names="Candy";
+        //candy.math=60;
+        //candy.english=88;
+        Students jack = new Students("Jack",60,85);
+        Students s = new Students();
+        candy.print();
+        jack.print();
+        s.print();
     }
 }
--- src/main/java/edu/fju/Students.java
+++ src/main/java/edu/fju/Students.java
@@ -4,4 +4,24 @@ public class Students {
     String names;
     int math;
     int english;
+    public Students(String names) {
+        this.names=names;
+    }
+    public Students(String names,int math,int english){
+        this(names);
+        //this.names=names;
+        this.math=math;
+        this.english=english;
+    }
+    public Students(){
+        this("John",60,50);
+    }
+    public void print(){
+        int average = (math+english)/2;
+        System.out.print(names+" "+math+" "+english+" "+average);
+        if (average<60){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 }
