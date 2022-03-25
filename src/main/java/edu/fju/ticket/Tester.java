e67ea924e7b0a5df84b7588d4a4649cd698cef1d
zhiyun <uouo911226@gmail.com>
2022-03-25 16:26:47 +0800

    Ticket

--- src/main/java/edu/fju/ticket/Tester.java
+++ src/main/java/edu/fju/ticket/Tester.java
@@ -1,8 +1,18 @@
 package edu.fju.ticket;
 
+import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args) {
         //Constructor,constant,value
-        Ticket ticket = new Ticket(Ticket.TAIPEI_STATION,Ticket.KAOHSIUNG_STATION);
+        Ticket ticket = new Ticket(Station.TAIPEI_STATION,Station.TAICHUNG_STATION);
+        Scanner scanner = new Scanner(System.in);
+        Station_startstation = null;
+        int startStation = Integer.getInteger(scanner.next());
+        switch (startStation){
+            case 1:
+                Station_startstaion=Station.TAIPEI_STATION;
+                break;
+        }
     }
 }
--- src/main/java/edu/fju/ticket/Ticket.java
+++ src/main/java/edu/fju/ticket/Ticket.java
@@ -1,15 +1,15 @@
 package edu.fju.ticket;
 
 public class Ticket {
-    public static final int TAIPEI_STATION =100;
+    /*public static final int TAIPEI_STATION =100;
     public static final int TAICHUNG_STATION =200;
     public static final int KAOHSIUNG_STATION =300;
-
-    int start;
-    int destination;
+    */
+    Station start;
+    Station destination;
     int price;
 
-    public Ticket(int start, int destination){
+    public Ticket(Station start, Station destination){
         this.start=start;
         this.destination=destination;
     }
