5d8e658090fc9bcaf470c6c3993dc44d636aaf02
CHY <uouo911226@gmail.com>
2022-05-27 15:50:48 +0800

    GUI Guess Game

--- src/main/java/problem/checkrepetition/Repeat.java
+++
@@ -1,9 +0,0 @@
-package problem.checkrepetition;
-
-public class Repeat {
-    public static void main(String[] args) {
-        String name = "ZhiYun";
-        int count = 4;
-        String x=name.repeat(count);
-    }
-}
--- src/main/java/problem/listnode/Solution.java
+++
@@ -1,7 +0,0 @@
-package problem.listnode;
-
-public class Solution {
-    public static void main(String[] args) {
-
-    }
-}
