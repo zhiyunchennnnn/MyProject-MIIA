73924a1d39541579825aa920e15582bd464cff61
CHY <uouo911226@gmail.com>
2022-06-17 16:29:15 +0800

    FileReader

--- src/main/java/edu/fju/score/ScoringFile.java
+++ src/main/java/edu/fju/score/ScoringFile.java
@@ -1,5 +1,6 @@
 package edu.fju.score;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,14 +9,27 @@ public class ScoringFile {
     public static void main(String[] args) {
         try {
             FileReader fileReader = new FileReader("Scores");//throws
-            int data = fileReader.read();
-            System.out.println(data);
-            data = fileReader.read();
-            System.out.println(data);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = reader.readLine();
+            while (line!=null){
+//                System.out.println(line);
+                String[] token=line.split(",");
+                String name =token[0];
+                int english = Integer.parseInt(token[1]);
+                int math = Integer.parseInt(token[2]);
+                Students stu = new Students(name,english,math);
+                stu.print();
+                line=reader.readLine();
+            }
+//            int data = fileReader.read();
+//            while (data!=-1){
+//                System.out.print((char) data);
+//                data= fileReader.read();
+
         }catch (FileNotFoundException e ){
-            System.out.println("檔案讀取失敗");
+            System.out.println("");
         }catch (IOException i){
-            System.out.println("資料讀取失敗");
+            System.out.println("");
         }
         System.out.println("Testing");
     }
